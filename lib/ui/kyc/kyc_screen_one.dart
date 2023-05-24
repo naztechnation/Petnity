e05b89e27c6ee09eab224eca53890628facc029e
Premium Ewsop <premiumewsop@Premiums-MacBook-Pro.local>
2023-05-24 17:30:47 +0100

    started kyc page

--- lib/ui/kyc/kyc_screen_one.dart
+++ lib/ui/kyc/kyc_screen_one.dart
@@ -16,26 +16,41 @@ class KycScreenOne extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           height: screenSize(context).height,
+          width: screenSize(context).width,
+
         decoration: BoxDecoration(
                 gradient: LinearGradient(
                     colors: [AppColors.scaffoldColor, Colors.red.shade50],
                     begin: Alignment.topRight,
                     end: Alignment.topLeft)),
-          child: Column(children: [
-            SizedBox(
-                height: screenSize(context).height * 0.15,
-              ),
-        
-                CustomText(
-                                    textAlign: TextAlign.center,
-                                    maxLines: 2,
-                                    text: 'Pet type',
-                                    weight: FontWeight.w700,
-                                    size: 32,
-                                    fontFamily: AppStrings.montserrat,
-                                    color: Colors.black,
-                                  ),
-          ],),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              SizedBox(
+                  height: 58,
+                ),
+                  
+                  CustomText(
+                                      textAlign: TextAlign.left,
+                                      maxLines: 2,
+                                      text: 'Pet type',
+                                      weight: FontWeight.w700,
+                                      size: 24,
+                                      fontFamily: AppStrings.montserrat,
+                                      color: Colors.black,
+                                    ),
+
+                                     SizedBox(
+                  height: 20,
+                ),
+                Material(child: Container(
+                  height: 77,
+                  width: 155,
+                  decoration: BoxDecoration()))
+            ],),
+          ),
         ),
       ),
     );
