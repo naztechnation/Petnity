f1984628ecd651d752eb2fa2b60482920fa0931e
Scarface <agbo.raph123@gmail.com>
2023-06-04 20:15:39 +0100

    changes

---
+++ lib/ui/notfications_pages/session_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SessionDetailsScreen extends StatelessWidget {
+  const SessionDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      )),
+    );
+  }
+}
