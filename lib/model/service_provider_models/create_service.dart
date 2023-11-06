07ba4dbfdf9aa02b226a5ca7e34d8662b45c7aff
Buffy Joe <okaforbestkid@gmail.com>
2023-11-06 01:16:31 -0800

    service provider api started

---
+++ lib/model/service_provider_models/create_service.dart
@@ -0,0 +1,107 @@
+import 'order_list.dart';
+
+class CreateOrder {
+  bool? status;
+  String? message;
+  Order? order;
+
+  CreateOrder({this.status, this.message, this.order});
+
+  CreateOrder.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    order = json['order'] != null ? new Order.fromJson(json['order']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['message'] = this.message;
+    if (this.order != null) {
+      data['order'] = this.order!.toJson();
+    }
+    return data;
+  }
+}
+
+class Order {
+  int? id;
+  String? pickupLocation;
+  String? pickupTime;
+  String? dropoffTime;
+  bool? shouldNotify;
+  String? fee;
+  String? purchaseId;
+  bool? isPaid;
+  bool? isAccepted;
+  bool? isRejected;
+  bool? isOngoing;
+  bool? isCompleted;
+  bool? paymentReleased;
+  String? dateCreated;
+  int? profile;
+  Agent? agent;
+  Package? package;
+
+  Order(
+      {this.id,
+      this.pickupLocation,
+      this.pickupTime,
+      this.dropoffTime,
+      this.shouldNotify,
+      this.fee,
+      this.purchaseId,
+      this.isPaid,
+      this.isAccepted,
+      this.isRejected,
+      this.isOngoing,
+      this.isCompleted,
+      this.paymentReleased,
+      this.dateCreated,
+      this.profile,
+      this.agent,
+      this.package});
+
+  Order.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    pickupLocation = json['pickup_location'];
+    pickupTime = json['pickup_time'];
+    dropoffTime = json['dropoff_time'];
+    shouldNotify = json['should_notify'];
+    fee = json['fee'];
+    purchaseId = json['purchase_id'];
+    isPaid = json['is_paid'];
+    isAccepted = json['is_accepted'];
+    isRejected = json['is_rejected'];
+    isOngoing = json['is_ongoing'];
+    isCompleted = json['is_completed'];
+    paymentReleased = json['payment_released'];
+    dateCreated = json['date_created'];
+    profile = json['profile'];
+     agent = json['agent'] != null ? new Agent.fromJson(json['agent']) : null;
+   package =
+        json['package'] != null ? new Package.fromJson(json['package']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['pickup_location'] = this.pickupLocation;
+    data['pickup_time'] = this.pickupTime;
+    data['dropoff_time'] = this.dropoffTime;
+    data['should_notify'] = this.shouldNotify;
+    data['fee'] = this.fee;
+    data['purchase_id'] = this.purchaseId;
+    data['is_paid'] = this.isPaid;
+    data['is_accepted'] = this.isAccepted;
+    data['is_rejected'] = this.isRejected;
+    data['is_ongoing'] = this.isOngoing;
+    data['is_completed'] = this.isCompleted;
+    data['payment_released'] = this.paymentReleased;
+    data['date_created'] = this.dateCreated;
+    data['profile'] = this.profile;
+    data['agent'] = this.agent;
+    data['package'] = this.package;
+    return data;
+  }
+}
